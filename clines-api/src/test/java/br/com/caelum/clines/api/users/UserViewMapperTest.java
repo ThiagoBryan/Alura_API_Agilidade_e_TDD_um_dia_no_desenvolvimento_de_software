68628244ffe735c3e0e6f129c7b96a2afc412aa3
Thiago <tthurler10@gmail.com>
2022-09-07 18:26:41 -0300

    Teste Integração

--- clines-api/src/test/java/br/com/caelum/clines/api/users/UserViewMapperTest.java
+++ clines-api/src/test/java/br/com/caelum/clines/api/users/UserViewMapperTest.java
@@ -11,19 +11,18 @@ class UserViewMapperTest {
 	private final String NAME = "FULANO";
 	private final String EMAIL = "fulano@teste.com";
 	private final String PASSWORD = "12345";
-	
+
 	private UserViewMapper mapper;
-		
+
 	@Test
 	void shouldConvertUserToUserView() {
-		var user = new User(NAME,EMAIL,PASSWORD); //criar um objeto
-		mapper = new UserViewMapper();//instanciar o mapper para testar
-		
+		var user = new User(NAME, EMAIL, PASSWORD); // criar um objeto
+		mapper = new UserViewMapper();// instanciar o mapper para testar
+
 		var userView = mapper.map(user);// devolve o metodo em userView
 		assertEquals(NAME, userView.getName());// faz os asserts para teste
 		assertEquals(EMAIL, userView.getEmail());
-		
-		
+
 	}
 
 }
